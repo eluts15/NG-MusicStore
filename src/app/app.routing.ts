882d631c141836762cc4c88af601653d5f943f28
eluts <eluts15@gmail.com>
2017-07-13 10:47:35 -0700

    add an About component that displays info about spotify.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,18 @@
 import { ModuleWithProviders } from  '@angular/core';
 import { Routes, RouterModule } from  '@angular/router';
 import { SplashComponent } from './splash/splash.component';
+import { AboutComponent } from './about/about.component';
 
 const appRoutes: Routes = [
 
   {
     path: '',
     component: SplashComponent
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
   },
 
 
