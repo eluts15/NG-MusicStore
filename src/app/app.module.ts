a0b153f074422247a2d2a3b00313e1b8f54ff97a
eluts <eluts15@gmail.com>
2017-07-13 10:12:48 -0700

    add marketplace and about component boilerplate. Add required references where required.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
 
 import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AboutComponent } from './about/about.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashComponent
+    SplashComponent,
+    MarketplaceComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
